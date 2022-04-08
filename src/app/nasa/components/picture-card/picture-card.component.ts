7b9f96ef0e3391caf7521e01ad4e02570aa269ee
Carlos Hidalgo Risco <carlos.hidalgo@atmira.com>
2022-04-08 13:00:52 +0200

    feat: add component picture-card and styles

---
+++ src/app/nasa/components/picture-card/picture-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-picture-card',
+  templateUrl: './picture-card.component.html',
+  styleUrls: ['./picture-card.component.scss']
+})
+export class PictureCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/nasa/nasa.module.ts
+++ src/app/nasa/nasa.module.ts
@@ -5,12 +5,14 @@ import { NasaRoutingModule } from './nasa-routing.module';
 import { DetailComponent } from './pages/detail/detail.component';
 import { HttpClientModule } from '@angular/common/http';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PictureCardComponent } from './components/picture-card/picture-card.component';
 
 
 @NgModule({
   declarations: [
     DetailComponent,
-    DashboardComponent
+    DashboardComponent,
+    PictureCardComponent
   ],
   imports: [
     CommonModule,

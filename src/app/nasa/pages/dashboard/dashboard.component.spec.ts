69244841a0b0f4e8d5b545386d62aaae3ac475ce
Carlos Hidalgo Risco <carlos.hidalgo@atmira.com>
2022-04-19 09:01:36 +0200

    feat: add test to know if The list of APODs contain 6 elements

--- src/app/nasa/pages/dashboard/dashboard.component.spec.ts
+++ src/app/nasa/pages/dashboard/dashboard.component.spec.ts
@@ -1,8 +1,47 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, tick } from '@angular/core/testing';
 import { DashboardComponent } from './dashboard.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
+import { NasaApodResponse } from '../../interfaces/nasaApodResponse.interface';
+
+const mockAPODResponse: NasaApodResponse[] = [
+  {
+    date: new Date('2022-04-03'),
+    explanation: 'This is the first APOD',
+    title: 'APOD 1',
+    url: 'https://apod.nasa.gov/apod/image/2204/CmbDipole_cobe_960.jpg',
+  },
+  {
+    date: new Date('2022-04-04'),
+    explanation: 'This is the second APOD',
+    title: 'APOD 2',
+    url: 'https://apod.nasa.gov/apod/image/2204/VortexAurora_Suarez_1080.jpg',
+  },
+  {
+    date: new Date('2022-04-05'),
+    explanation: 'This is the third APOD',
+    title: 'APOD 3',
+    url: 'https://apod.nasa.gov/apod/image/2204/Calif2Pleiades_Krcmarek_1080.jpg',
+  },
+  {
+    date: new Date('2022-04-06'),
+    explanation: 'This is the fourth APOD',
+    title: 'APOD 4',
+    url: 'https://apod.nasa.gov/apod/image/2204/LensedStar_Hubble_960.jpg',
+  },
+  {
+    date: new Date('2022-04-07'),
+    explanation: 'This is the fifth APOD',
+    title: 'APOD 5',
+    url: 'https://apod.nasa.gov/apod/image/2204/M24_APOD_GabrielRodriguesSantosAPOD1100.jpg',
+  },
+  {
+    date: new Date('2022-04-08'),
+    explanation: 'This is the sixth APOD',
+    title: 'APOD 6',
+    url: 'https://apod.nasa.gov/apod/image/2204/HaleBoppSeip_c1024.jpg',
+  }
+]
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -46,11 +85,12 @@ describe('DashboardComponent', () => {
     expect(compiled.querySelector('app-loading-logo')).toBeNull();
   });
 
-  // it('The list of APOD should be contain 6 elements', () => {
-  //   component.isLoading = false;
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelectorAll('app-picture-card').length).toBe(6);
-  // } );
+  it('The list of APOD should be contain 6 elements', () => {
+    component.isLoading = false;
+    component.lastSixAPOD = mockAPODResponse;
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelectorAll('app-picture-card').length).toBe(6);
+  } );
   
-
 });
